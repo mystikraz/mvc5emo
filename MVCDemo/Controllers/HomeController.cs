79bc5736033030ac212db342c9bcfdacc7c8c035
Raj Tandukar <royal_raj@outlook.com>
2018-01-27 15:08:06 +0545

    add jquery-ajax to contact view for sending message

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -12,18 +12,25 @@ namespace MVCDemo.Controllers
         {
             return View();
         }
-        [CustomAuthorize(Roles ="Admin")]
+        [CustomAuthorize(Roles = "Admin")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-        [CustomAuthorize(Roles = "Sales")]
+        [CustomAuthorize(Roles = "Admin")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            //TODO: send the message to HQ
+            ViewBag.TheMessage = "Thanks, we got your message!";
             return View();
         }
     }
